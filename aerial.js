18df53d4822d0ced1d20c5733c931d4dfbd288b9
Keagan Lueck <keaganlueck@gmail.com>
2025-01-27 15:30:44 -0600

    changed phone number

--- aerial.js
+++ aerial.js
@@ -83,7 +83,7 @@ function baseText() {
   style.fontWeight = "normal";
   style.fontStyle = "normal";
   style.color = "#45a29e";
-  phone.textContent = "phone number: 507-430-0823";
+  phone.textContent = "phone number: 507-430-5299";
 }
 function ebaseText() {
   estyle.fontWeight = "normal";
@@ -118,7 +118,7 @@ function mcopyTextHint() {
 function copyText() {
   style.fontWeight = "normal";
   style.fontStyle = "italic";
-  navigator.clipboard.writeText("507-430-0823");
+  navigator.clipboard.writeText("507-430-5299");
   phone.textContent = "COPIED";
 }
 function ecopyText() {
@@ -194,155 +194,6 @@ function getPosSuccess(pos) {
         "sm-ct",
         "semi",
       ];
-      function pressed(e) {
-        e.preventDefault(e); // Prevents default action (redirect) when the form is submitted
-        var text = document.getElementById("jr").value;
-        var output = document.getElementById("output");
-        switch (text) {
-          //other stuff
-          case "tesla notes":
-            var bgImg = document.querySelector("#li");
-            bgImg.classList.add("background-images");
-            document.getElementById("li").style.backgroundImage = "url('')";
-            tesla.innerHTML = "";
-            header.innerHTML = "";
-            jr.style.display = "none";
-            hide.style.display = "none";
-            window.location.replace((url = "tesla notes.html"));
-            break;
-          case "science notes":
-            window.location.replace((url = "particle theory test review.html"));
-            break;
-          case "basketball highlights":
-            window.location.replace("basketball-highlights.html");
-            break;
-          case "screen size":
-            window.location.replace((url = "screen-size.html"));
-            break;
-          case "bitcoin":
-            var walletAddress = "bc1qdkcmdk3wsyt2hgj48cgyrr70hf5dmag4l204es";
-            header.innerHTML = walletAddress;
-            navigator.clipboard.writeText(walletAddress);
-            setTimeout(() => {
-              alert("Wallet address saved to clipboard");
-            }, 250);
-            console.log(
-              "Success, bitcoin wallet address automatically copied to clipboard"
-            );
-            break;
-          case "suggestion":
-            window.location.replace((url = "suggestion.html"));
-            break;
-          case "slime":
-            const sneak = new Audio("sneak.mp3");
-            sneak.play();
-            break;
-          case "errday":
-            const errday = new Audio("errday.mp3");
-            errday.play();
-            break;
-          case "pics":
-            window.location.replace((url = "pics.html"));
-            break;
-          case "mask law":
-            window.location.replace((url = "mask.html"));
-            break;
-          case "cybertruck v ford":
-            window.location.replace((url = "cybertruck.html"));
-            break;
-          case "youtube playlist":
-            window.location.replace(
-              (url =
-                "https://youtube.com/playlist?list=PL9ohnLdAHcjmM6Dx9unBDW3Kdi42ddPdf")
-            );
-            break;
-          case "bbb chapter 1":
-            window.location.replace((url = "blackjack-bluebook-chapter1.html"));
-            break;
-          case "bbb chapter 2":
-            window.location.replace((url = "blackjack-bluebook-chapter2.html"));
-            break;
-          case "bbb chapter 3":
-            window.location.replace((url = "blackjack-bluebook-chapter3.html"));
-            break;
-          case "pc specs":
-            window.location.replace(
-              (url = "https://pcpartpicker.com/user/klueck21/saved/bPNJf7")
-            );
-            break;
-          case "keyboard specs":
-            window.location.replace("keyboard-specs.html");
-            break;
-          case "desk setup":
-            window.location.replace((url = "desk-setup.html"));
-            break;
-          case "rubiks guide":
-            window.location.replace((url = "rubiks-guide.pdf"));
-            break;
-          case "cube in cube":
-            window.location.replace(
-              (url =
-                "https://ruwix.com/the-rubiks-cube/rubiks-cube-patterns-algorithms/")
-            );
-            break;
-          case "center swap":
-            window.location.replace(
-              (url =
-                "https://sites.psu.edu/phelpsinstructionset/tricks/center-cube-swap/")
-            );
-            break;
-          case "groovebook":
-            window.location.replace((url = "groovebook.html"));
-            break;
-          case "blog":
-            window.location.replace((url = "blog.html"));
-            break;
-          case "tesla counter":
-            window.location.replace((url = "tesla-counter.html"));
-            break;
-          case "model y vs expedition":
-            window.location.replace((url = "MY-06E.html"));
-            break;
-          case "s10e vs iphone 8":
-            window.location.replace((url = "s10e-compared.html"));
-            break;
-          case "changelog":
-            window.location.replace((url = "changelog.html"));
-            break;
-          case "v1":
-            window.location.replace((url = "v1.html"));
-            break;
-          case "home":
-            window.location.replace((url = "index.html"));
-            break;
-          case "photo gallery":
-            window.location.replace((url = "pics-gallery.html"));
-            break;
-          case "contact":
-            window.location.replace((url = "contact.html"));
-            break;
-          case "contact":
-            window.location.replace((url = "phone.html"));
-            break;
-          case "photography":
-            window.location.replace((url = "photography-class.html"));
-            break;
-          case "convert":
-            window.location.replace((url = "celsius-fahrenheit.html"));
-            break;
-          default:
-            tesla.innerHTML = "";
-            header.innerHTML = greeting;
-            document.getElementById("header").style.color = "#45a29a";
-            document.getElementById("header").style.fontFamily = "";
-            document.getElementById("button").style.backgroundColor = "";
-            document.getElementById("footer").style.display = "inline-block";
-            document.getElementById("footer").style.color = "white";
-
-            break;
-        }
-        jr.value = "";
-      }
       //my bday wallpaper
       var now = new Date();
       let keaganbday = new Date("July 15");
